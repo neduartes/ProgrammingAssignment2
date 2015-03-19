d2bf315d469bcd066be87e55ac4f1c8010bfd1ce
Nestor Eduartes <neduarteslar@mail.usf.edu>
2015-03-18 23:13:04 -0400

    First functional version of Assignment

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,32 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(mat = matrix()) {
+  inverse <- NULL
+  set <- function(y) {
+    mat <<- y
+    inverse <<- NULL
+  }
+  get <- function() mat
+  setinverse <- function(mat) inverse <<- solve(mat)
+  getinverse <- function() inverse
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(matrix, ...) {
+  inverse <- matrix$getinverse()
+  if(!is.null(inverse)) {
+    message("getting cached data")
+    return(inverse)
+  }
+  data <- matrix$get()
+  inverse <- solve(data)
+  matrix$setinverse(inverse)
+  inverse
         ## Return a matrix that is the inverse of 'x'
 }
